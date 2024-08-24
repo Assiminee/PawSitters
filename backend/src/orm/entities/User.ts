77d520b49c9789f2adebf55b620caa7f08f5b546
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-08-24 16:27:47 +0100

    Added some more details

--- backend/src/orm/entities/User.ts
+++ backend/src/orm/entities/User.ts
@@ -5,6 +5,15 @@ import {Address} from "./Address";
 import {Certification} from "./Certification";
 import {Role} from "./Role";
 import {Review} from "./Review";
+import {
+    IsDate,
+    IsDecimal,
+    IsEmail,
+    IsNotEmpty,
+    IsOptional,
+    IsPhoneNumber,
+    MinLength
+} from "class-validator";
 
 @Entity()
 export class User extends BaseModel {
@@ -14,37 +23,52 @@ export class User extends BaseModel {
         this.lname = '';
         this.email = '';
         this.phone = '';
+        this.password = '';
         this.gender = '';
-        this.birthday = null;
-        this.fee = 0;
         this.bankAccountNumber = '';
+        this.birthday = null;
         this.photos = null;
         this.address = null;
         this.certifications = null;
         this.roles = null;
         this.reviewsReceived = null;
         this.reviewsGiven = null;
+        this.fee = 0;
     }
 
     @Column("varchar", {length: 50})
+    @IsNotEmpty()
     fname: string
 
     @Column("varchar", {length: 50})
+    @IsNotEmpty()
     lname: string
 
     @Column({type: "varchar", length: 255, unique: true})
+    @IsNotEmpty()
+    @IsEmail()
     email: string
 
     @Column({type: "varchar", length: 20, unique: true, nullable: true})
+    @IsOptional()
+    @IsPhoneNumber()
     phone: string
+
+    @Column({type: "varchar", length: 255})
+    @IsNotEmpty()
+    @MinLength(10)
+    password: string
 
     @Column({type: "enum", enum: ["Female", "Male"]})
     gender: string
 
     @Column({type: "date", update: false})
+    @IsDate()
     birthday: Date | null
 
     @Column({type: "decimal", precision: 10, scale: 2, nullable: true})
+    @IsOptional()
+    @IsDecimal()
     fee: number
 
     @Column({type: "varchar", length: 34, nullable: true, unique: true})
@@ -56,7 +80,10 @@ export class User extends BaseModel {
     @OneToOne(() => Address, (address: Address) => address.user, {nullable: true, onDelete: "CASCADE"})
     address: Address | null
 
-    @OneToMany(() => Certification, (certification: Certification) => certification.user, {nullable: true, onDelete: "CASCADE"})
+    @OneToMany(() => Certification, (certification: Certification) => certification.user, {
+        nullable: true,
+        onDelete: "CASCADE"
+    })
     certifications: Certification[] | null
 
     @ManyToMany(() => Role, (role: Role) => role.users)
