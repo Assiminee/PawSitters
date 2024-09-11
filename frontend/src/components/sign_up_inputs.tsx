936cce9732258bd927e42934d4a6ed60ec2bc9c1
Sandy Obare <obaresandy@gmail.com>
2024-09-11 04:11:29 +0300

    sign_up_inputs.tsx initial commit: sign_up form input components

---
+++ frontend/src/components/sign_up_inputs.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function SignUpInputs(props: any) {
+    const [focused, setFocused] = useState(false);
+    const {label, errorMessage, onChange, id, ...inputProps} = props;
+
+    const handleFocus = (e: React.FocusEventHandler<HTMLInputElement>) => {
+      setFocused(true);
+    }
+  return (
+    <div className='bg-slate-300 border-b border-white px-2 py-1'>
+      <label className='block text-sm text-gray-600 font-semibold'>{label}</label>
+      <input
+        {...inputProps}
+        className='signup_fields'
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() => inputProps.name === "confirmPassword" && setFocused(true)}
+        focused={focused.toString()}/>
+      <span className="error text-[0.688rem] text-red-500 hidden">{errorMessage}</span>
+    </div>
+  )
+}
+
+export default SignUpInputs
